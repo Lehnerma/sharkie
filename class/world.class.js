fedc7262f6976cd033c8d9a008f4d42eb5171c32
Max Lehner <150380803+Lehnerma@users.noreply.github.com>
2026-06-17 07:54:11 +0200

    Lets add stuff to the page to be frindly to  the eyes.

--- class/world.class.js
+++ class/world.class.js
@@ -1,13 +1,14 @@
 class World{
     canvas;
     keyboard;
-    ctx; 
+    ctx;
+    level = level1;
 
     constructor(canvas, keyboard){
         this.canvas = canvas;
         this.ctx = canvas.getContext("2d"); // get the coordinate system of the canvas. 
         this.keyboard = keyboard // set the variables for the btn by clicking to the an by relese to false back.
-        
+
     }
 
     draw(){
